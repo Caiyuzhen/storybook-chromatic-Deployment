7145d424e586037ded61600b1ea6725e17226d2f
yuzhen <caiyuzhen00@gmail.com>
2022-07-08 17:47:08 +0800

    fix

--- storybook-static/service-worker.js
+++ storybook-static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.7ae134be8ba147259af644def7c1e64d.js"
+  "precache-manifest.9152677f390ebffea77d755397c58c0b.js"
 );
 
 self.addEventListener('message', (event) => {
