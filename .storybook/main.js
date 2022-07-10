dfa3a1a66e3a320aa0b3eaf217239ec5d7db94a4
Zen <caiyuzhen00@gmail.com>
2022-07-10 10:10:08 +0800

    fix10

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,4 +5,12 @@ module.exports = {
     '@storybook/addon-actions',
     '@storybook/addon-links',
   ],
+  refs: {
+    "chromatic-published-Storybook": {
+      // The title of your Storybook
+      title: "Design System",
+      // The url provided by Chromatic when it was published
+      url: "https://ZenStorybook.chromatic.com",
+    },
+  },
 };
